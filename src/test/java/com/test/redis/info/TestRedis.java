711ccbee280876eb70c2916c8f34add52d6fc6b7
luoxn28 <luoxn28@163.com>
2017-11-01 22:06:16 +0800

    初始化redis-info工程

---
+++ src/test/java/com/test/redis/info/TestRedis.java
@@ -0,0 +1,32 @@
+package com.test.redis.info;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = {com.luo.redis.info.Application.class})
+public class TestRedis {
+
+    @Autowired
+    private StringRedisTemplate template;
+
+    @Test
+    public void testRedis() {
+        System.out.println(template);
+
+        ValueOperations<String, String> ops = this.template.opsForValue();
+        String key = "spring.boot.redis.test";
+        if (!this.template.hasKey(key)) {
+            ops.set(key, "foo");
+        }
+        System.out.println("Found key " + key + ", value=" + ops.get(key));
+    }
+
+}
