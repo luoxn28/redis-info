fe29c20b0adab6386a5c0284c2ed386e89ca018e
luoxn28 <luoxn28@163.com>
2017-11-02 09:54:44 +0800

    直接执行redis命令

--- src/test/java/com/test/redis/info/TestRedis.java
+++ src/test/java/com/test/redis/info/TestRedis.java
@@ -4,11 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Properties;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -35,10 +40,35 @@ public class TestRedis {
     public void testObject() {
         ValueOperations<String, Person> ops = redisTemplate.opsForValue();
         String key = "person";
-        if (redisTemplate.hasKey(key)) {
+        if (!redisTemplate.hasKey(key)) {
             ops.set(key, new Person("luo", 25));
         }
         System.out.println("Found key " + key + ", value=" + ops.get(key));
+
+        System.out.println(redisTemplate.opsForValue().get("info"));
+    }
+
+    @Test
+    public void testCallback() {
+
+        final Properties[] result = {null};
+
+        redisTemplate.execute(new RedisCallback<Object>() {
+            @Override
+            public Object doInRedis(RedisConnection connection) throws DataAccessException {
+                Properties properties = connection.info();
+                System.out.println(properties);
+
+                properties = connection.info("CPU");
+                System.out.println(properties);
+
+                result[0] = properties;
+
+                return null;
+            }
+        });
+
+        System.out.println(result[0]);
     }
 
 }
